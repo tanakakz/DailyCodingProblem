c494eb1bf4a74c5dfb517e65465ff4e4b3dd4a76
tanakakz <tanakakz.feb.23@gmail.com>
2021-01-05 22:29:01 +0900

    first commit.

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod y2021;
+
+fn main() {
+    println!("Daily Coding Problem");
+    y2021::m01::d05::main();
+}
---
+++ src/y2021/m01/d05.rs
@@ -0,0 +1,19 @@
+use proconio::input;
+
+pub fn main() {
+    input! {
+        a: i32,
+        b: i32,
+    }
+
+    println!("{}", f(a).max(f(b)));
+}
+
+fn f(mut n: i32) -> i32 {
+    let mut res = 0;
+    while n != 0 {
+        res += n % 10;
+        n /= 10;
+    }
+    res
+}
