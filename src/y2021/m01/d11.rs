e949b0dc07ff6a876f88cb34b747d877507e5c3a
tanakakz <tanakakz.feb.23@gmail.com>
2021-01-11 23:06:49 +0900

    daily coding at 2021/1/11

---
+++ src/y2021/m01/d11.rs
@@ -0,0 +1,8 @@
+// Good morning! Here's your coding interview problem for today.
+// おはようございます！今日もお疲れ様でした。今日のコーディング面接問題です。
+// This problem was asked by Google.
+// この問題はGoogleから出題されました。
+// An XOR linked list is a more memory efficient doubly linked list. Instead of each node holding next and prev fields, it holds a field named both, which is an XOR of the next node and the previous node. Implement an XOR linked list; it has an add(element) which adds the element to the end, and a get(index) which returns the node at index.
+// XORリンクリストは、よりメモリ効率の高い二重リンクリストです。各ノードが次のノードと前のノードのフィールドを保持する代わりに、次のノードと前のノードのXORである両方という名前のフィールドを保持します。XOR リンクリストを実装します。これは、要素を末尾に追加する add(element) と、インデックスのノードを返す get(index) を持ちます。
+// If using a language that has no pointers (such as Python), you can assume you have access to get_pointer and dereference_pointer functions that converts between nodes and memory addresses.
+// ポインタを持たない言語（Pythonなど）を使用している場合は、ノードとメモリアドレスを変換するget_pointerとdereference_pointer関数にアクセスできることを前提にしてください。
