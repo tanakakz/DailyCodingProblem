19519dbc8718f8e9f776e98cf29c90391e4033ca
tanakakz <tanakakz.feb.23@gmail.com>
2021-01-15 23:30:49 +0900

    daily coding at 2021/1/15

---
+++ src/y2021/m01/d15.rs
@@ -0,0 +1,7 @@
+// Good morning! Here's your coding interview problem for today.
+// おはようございます！今日もお疲れ様でした。今日のコーディング面接問題です。
+// This problem was asked by Apple.
+// この問題はAppleからの質問です。
+// Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
+// 関数fと整数nを取り込み、nミリ秒後にfを呼び出すジョブスケジューラを実装してください。
+
