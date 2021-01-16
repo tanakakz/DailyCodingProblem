eeff386b3c8a6094a3ee76d08167860cee8e0580
tanakakz <tanakakz.feb.23@gmail.com>
2021-01-16 23:01:54 +0900

    daily coding at 2021/1/16

--- src/y2021/m01/d14.rs
+++ src/y2021/m01/d14.rs
@@ -1,9 +1,34 @@
 // Good morning! Here's your coding interview problem for today.
-//
+// おはようございます！今日のコーディング面接問題です。
 // This problem was asked by Airbnb.
-//
+// この問題はAirbnbに質問されました。
 // Given a list of integers, write a function that returns the largest sum of non-adjacent numbers. Numbers can be 0 or negative.
-//
+// 整数のリストが与えられたとき，隣接しない数の最大和を返す関数を書きなさい．各数値には0や負の値を指定できます。
 // For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1, 1, 5] should return 10, since we pick 5 and 5.
-//
+// 例えば、[2, 4, 6, 2, 5]は13を返します。5, 1, 1, 5] は 10 を返します。
 // Follow-up: Can you do this in O(N) time and constant space?
+// フォローアップ：O(N)の時間と一定の空間でこれができますか？
+
+use proconio::input;
+use std::arch::x86_64::_xabort;
+
+#[allow(dead_code)]
+pub fn main() {
+    input! {
+        n: i32,
+        g: [i32; n]
+    }
+    println!("input: {:?}", g);
+    println!("output: {}", f(g));
+}
+
+fn f(&mut g: Vec<i32>) -> i32 {
+    // index: n+0 1 2 3 4 | 5 6 7 8 9
+    //    a)    x   x   x | max(c,d,e)
+    //    b)    x     x   | max(a,b,e)
+    //    c)      x   x   | max(a,b,e)
+    //    d)      x     x | max(c,d,e)
+    // ** e)        x   x | max(c,d,e)
+    let mut smax: i32 = 0;
+
+}
---
+++ src/y2021/m01/d16.rs
@@ -0,0 +1,9 @@
+// Good morning! Here's your coding interview problem for today.
+// おはようございます！今日もお疲れ様でした。今日のコーデ面接問題です。
+// This problem was asked by Twitter.
+// この問題はTwitterで出題されました。
+// Implement an autocomplete system. That is, given a query string s and a set of all possible query strings, return all strings in the set that have s as a prefix.
+// オートコンプリートシステムを実装しなさい。つまり、クエリ文字列sと、可能なクエリ文字列の集合が与えられたとき、その集合の中でsを接頭辞として持つ文字列をすべて返す。
+// For example, given the query string de and the set of strings [dog, deer, deal], return [deer, deal].
+// 例えば、クエリ文字列 de と文字列 [dog, deer, deal] の集合が与えられた場合、[deer, deal] を返します。
+// Hint: Try preprocessing the dictionary into a more efficient data structure to speed up queries.// ヒント: クエリを高速化するために、辞書をより効率的なデータ構造に前処理してみてください。
