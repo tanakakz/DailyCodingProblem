0c7d3aa321b48e40e706174e47a85fb9234b6a25
tanakakz <tanakakz.feb.23@gmail.com>
2021-01-18 20:44:03 +0900

    daily coding at 2021/1/18

---
+++ src/y2021/m01/d17.rs
@@ -0,0 +1,29 @@
+// Good morning! Here's your coding interview problem for today.
+//
+// This problem was asked by Amazon.
+//
+// There exists a staircase with N steps, and you can climb up either 1 or 2 steps at a time. Given N, write a function that returns the number of unique ways you can climb the staircase. The order of the steps matters.
+//
+// For example, if N is 4, then there are 5 unique ways:
+//
+// 1, 1, 1, 1
+// 2, 1, 1
+// 1, 2, 1
+// 1, 1, 2
+// 2, 2
+// What if, instead of being able to climb 1 or 2 steps at a time, you could climb any number from a set of positive integers X? For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.
+//
+// おはようございます！今日もお疲れ様でした。本日のコーディング面接問題です。
+//
+// この問題はAmazonからの質問です。
+//
+// N段の階段が存在し、一度に1段または2段の階段を登ることができます。Nが与えられたとき、階段を登ることができるユニークな方法の数を返す関数を書いてください。階段の順番が重要です。
+//
+// 例えば，Nが4であれば，5つのユニークな登り方があります．
+//
+// 1, 1, 1, 1
+// 2, 1, 1
+// 1, 2, 1
+// 1, 1, 2
+// 2, 2
+// 一度に1,2歩ずつ登れるのではなく、正の整数Xの集合から任意の数を登れるとしたらどうでしょうか？例えば、X = {1, 3, 5}の場合、一度に1, 3, 5歩ずつ登ることができます。
---
+++ src/y2021/m01/d18.rs
@@ -0,0 +1,15 @@
+// Good morning! Here's your coding interview problem for today.
+//
+// This problem was asked by Amazon.
+//
+// Given an integer k and a string s, find the length of the longest substring that contains at most k distinct characters.
+//
+// For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb".
+//
+// おはようございます！今日もお疲れ様でした。本日のコーディング面接問題です。
+//
+// この問題はAmazonで出題されたものです。
+//
+// 整数 k と文字列 s が与えられると、最大でも k 個の明瞭な文字を含む最長の部分文字列の長さを求めなさい。
+//
+// 例えば、s = "abcba "とk = 2が与えられているので、k個の異なる文字を含む最長の部分文字列の長さは "bcb "となります。
