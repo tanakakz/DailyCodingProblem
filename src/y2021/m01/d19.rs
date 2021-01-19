467514537b42843af9246e7798edda1715c80749
tanakakz <tanakakz.feb.23@gmail.com>
2021-01-19 23:32:17 +0900

    daily coding at 2021/1/19

---
+++ src/y2021/m01/d19.rs
@@ -0,0 +1,15 @@
+// Good morning! Here's your coding interview problem for today.
+//
+// This problem was asked by Google.
+//
+// The area of a circle is defined as πr^2. Estimate π to 3 decimal places using a Monte Carlo method.
+//
+// Hint: The basic equation of a circle is x2 + y2 = r2.
+//
+// おはようございます！今日もお疲れ様でした。今日のコーディング面接問題です。
+//
+// この問題はGoogleから出題されました。
+//
+// 円の面積をπr^2と定義します。モンテカルロ法を用いて、πを小数点以下3桁まで推定しなさい。
+//
+// ヒント：円の基本方程式はx2 + y2 = r2です。
